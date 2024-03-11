872b9acb6a25b67643af35600e0ab789366eea35
Abraham Quaye <quayeabraham29@gmail.com>
2024-03-10 23:57:45 -0600

    re-generated count matrices with prepDE.py3 and attr 'ref_gene_name'

--- scripts/shell_code/make_count_matrix.zsh
+++ scripts/shell_code/make_count_matrix.zsh
@@ -9,7 +9,7 @@ t_outdir=$i/count_matrix/trxpts_count_matrix.csv
 
 echo "Generating Count Matrix..."
 
-prepDE.py -i $i -l $l -p $p -s $s -g $g_outdir -t $t_outdir
+scripts/python/prepDE.py3 -i $i -l $l -p $p -s $s -g $g_outdir -t $t_outdir
 
 secs=$SECONDS
 hrs=$(( secs/3600 )); mins=$(( (secs-hrs*3600)/60 )); secs=$(( secs-hrs*3600-mins*60 ))
