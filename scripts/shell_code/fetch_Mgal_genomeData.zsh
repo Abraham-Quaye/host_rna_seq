49b5ee4ff49b50bd8dac0014a003b0a2d0b130b4
Abraham Quaye <quayeabraham29@gmail.com>
2024-04-11 13:17:38 -0600

    included the FASTA file for Mgal for download

--- scripts/shell_code/fetch_Mgal_genomeData.zsh
+++ scripts/shell_code/fetch_Mgal_genomeData.zsh
@@ -1,17 +1,31 @@
 #!/usr/bin/env zsh
 
 outdir=raw_files/annotations
-files=(GCF_000146605.3_Turkey_5.1_genomic.gtf.gz GCF_000146605.3_Turkey_5.1_gene_ontology.gaf.gz)
-names=(Mgallopavo_ncbi.gtf.gz Mgallopavo_GOncbi.gaf.gz)
+files=(GCF_000146605.3_Turkey_5.1_genomic.gtf.gz GCF_000146605.3_Turkey_5.1_gene_ontology.gaf.gz \
+GCF_000146605.3_Turkey_5.1_genomic.fna.gz )
+names=(Mgallopavo_ncbi.gtf.gz Mgallopavo_GOncbi.gaf.gz Mgallopavo_genome.fna.gz)
 
 for n in {1..3}; do
     echo "Downloading $files[$n]"
 
     wget -P $outdir https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/146/605/GCF_000146605.3_Turkey_5.1/${files[$n]}
-    mv $outdir/${files[$n]} $outdir/${names[$n]}
-
+    
+    if [[ $n < 3 ]];
+    then
+        mv $outdir/${files[$n]} $outdir/${names[$n]}
+    else
+         mv $outdir/${files[$n]} $outdir/${names[$n]}
+         mv $outdir/${names[$n]} raw_files/genome_file
+    fi
+    
     echo "$files[$n] Downloaded and Saved as $outdir/${names[$n]}"
 done
+echo "All files downloaded successfully"
+
+echo "Now unzipping FASTA file"
+gunzip raw_files/genome_file/Mgallopavo_genome.fna.gz
+
+echo "Unzipping complete"
 
 # echo "Downloading Meleagris_gallopavo.Turkey_5.1.111.gtf.gz from ENSEMBL"
 
